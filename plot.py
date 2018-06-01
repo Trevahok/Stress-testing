3a2fa808003f784893b031927e16d48173f13492
cloud <cloud@localhost.localdomain>
2018-06-01 10:53:49 +0530

    modify to plot codetest results

--- plot.py
+++ plot.py
@@ -8,18 +8,21 @@ data=list(map(lambda z:z.split('\n')[1:-1],f.read().split('#')))
 for node in data:
     for record in node:
         if 'Could' not in record:
-            x=literal_eval(record)[list(literal_eval(record).keys())[0]]['home page load time']
+            x=literal_eval(record)[list(literal_eval(record).keys())[0]]['writing code test time']
             hometime.append(x)
 count_dict={}
 for time in hometime:
-    count_dict[int(time)]=0
+    count_dict[int(time*10)]=0
 
 for time in hometime:
-    count_dict[int(time)]+=1
+    count_dict[int(time*10)]+=1
     
-#plt.scatter(range(len(hometime)),hometime)
+plt.scatter(range(len(hometime)),hometime)
 x=list(count_dict.keys())
 plt.xticks(x)
 plt.bar(x,height=list(count_dict.values()))
-plt.xlabel(fname)
+plt.title(fname)
+print(hometime)
+plt.xlabel('time => 1 unit = 0.1 secs') 
 plt.savefig(fname)
+plt.show()
